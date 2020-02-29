9608707ecd78303b799c7ebc5144be9fa2c332d9
am <am@ams-iMac.local>
2020-02-29 19:46:09 +0600

    add service

--- front-end/src/app/users/lists/lists/lists.component.ts
+++ front-end/src/app/users/lists/lists/lists.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
 import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { UserService } from '../../../_service/user.service'
 
 @Component({
   selector: 'user-lists',
@@ -12,15 +13,17 @@ export class ListsComponent implements OnInit {
   private userSubject = new Subject();
 
   users = this.userSubject.pipe(
-
+    debounceTime(250),
+    distinctUntilChanged(),
+    switchMap(userTag => this.userService.get(userTag))
   );
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
 
   searchUsers(value) {
-    
+    this.userSubject.next(value);
   }
 }
